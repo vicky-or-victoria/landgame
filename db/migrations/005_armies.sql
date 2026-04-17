304a59e96ce5cb224b1f6b53e982da4c9734ee98
vicky <jvitoclucero@gmail.com>
2026-04-18 01:26:16 +0300

    Update 005_armies.sql

--- db/migrations/005_armies.sql
+++ db/migrations/005_armies.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS armies (
-    id          SERIAL PRIMARY KEY,
-    owner_id    BIGINT REFERENCES players(discord_id) ON DELETE CASCADE,
-    name        TEXT,
-    location    TEXT REFERENCES tiles(coord),
-    created_at  TIMESTAMP DEFAULT NOW()
+    id              SERIAL PRIMARY KEY,
+    guild_id        BIGINT NOT NULL,
+    owner_id        BIGINT NOT NULL,
+    name            TEXT,
+    location        TEXT,
+    created_at      TIMESTAMP DEFAULT NOW()
 );
+CREATE INDEX IF NOT EXISTS armies_guild_owner ON armies(guild_id, owner_id);
